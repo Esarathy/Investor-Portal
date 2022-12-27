4e921968117478a738cc881f85ad67c01a3f0193
Esarathy <parthasarathy.elangovan-talentship.io>
2022-12-27 11:54:50 +0530

    Investor-portal

--- cypress/integration/examples/BDD/Reportspage/Reportspage_stepdef.js
+++ cypress/integration/examples/BDD/Reportspage/Reportspage_stepdef.js
@@ -1,12 +1,13 @@
 import { Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
 import Navigation from "../../../../support/Pageobject/Naviagtion";
 import Reports from "../../../../support/Pageobject/Reportspage";
+
 const navigation = new Navigation()
 const reports = new Reports()
 
 
+
 Given('User lands on the Dashboard', () => {
-    cy.viewport(1400, 1093)
     cy.visit(Cypress.env('url'))
     cy.url().should('include', '.cloudfront.net/dashboard')
     cy.clearCookies()
@@ -29,21 +30,21 @@ When('User clicks the Quarterly toggle button', () => {
 And('User selects the year and quarter drop down', () => {
     cy.get('#mat-select-4').click()
     cy.get('#mat-option-6').click()
-    reports.getmenutitle().should('include.text','2022')
-   
+    reports.getmenutitle().should('include.text', '2022')
+
 
     cy.get('#mat-select-6').click()
     cy.get('#mat-option-11').click()
-    reports.getmenutitle().should('include.text','Q4')
+    reports.getmenutitle().should('include.text', 'Q4')
 
 })
 Then('User should get the following documents', () => {
-cy.get('mat-selection-list > mat-list-option').click({multiple:true})
+    cy.get('mat-selection-list > mat-list-option').click({ multiple: true })
 
 })
-And('User should preview the pdf', () => {
-    cy.get('#webviewer-1')
-    cy.get('.DocumentContainer').should('be.visible')
+And('User able to see the pdf', () => {
+    cy.get('.pageContainer')
+    
 
 })
 When('User clicks the Annually in the toggle button', () => {
@@ -58,10 +59,11 @@ And('Selects the year', () => {
     cy.get('.mat-list-text > div').then(($btn1) => {
         const txt1 = $btn1.text()
         cy.get('.mat-list-text > div').click()
-       
+
     })
 })
 And('User should preview the pdf', () => {
-    cy.get('#webviewer-2')
-    cy.get('.DocumentContainer').should('be.visible')
+    cy.get('.pageWidgetContainer1').click()
+    
+
 })
