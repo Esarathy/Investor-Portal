f7a4e31b13b65c25a1981d1aa76e76d69538293b
Esarathy <parthasarathyelangovan@talentship.io>
2023-05-31 13:31:05 +0530

    common function for login

--- cypress/integration/Common/Common.js
+++ cypress/integration/Common/Common.js
@@ -1,8 +1,9 @@
 import Capitalaccount from "../../support/Pageobject/Capitalaccountpage"
+import Loginpage from "../../support/Pageobject/Loginpage";
 
 
 const capitalaccount = new Capitalaccount
-
+const loginpage = new Loginpage();
 
 
 class Common {
@@ -12,7 +13,14 @@ class Common {
     capitalaccount.getyearmonthdate().contains(month).click()
     capitalaccount.getyearmonthdate().contains(date).click()
   }
+  login(url,username,password) {
+    cy.visit(Cypress.env(url));
+    loginpage.getusername().type(username);
+    loginpage.getpassword().type(password);
+    loginpage.getsignin().click();
+    Cypress.on("uncaught:exception", (err, runnable) => {
+      return false;
+    })
+  }
 }
-
-
 export default Common
